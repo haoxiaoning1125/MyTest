5b12fe443957c20ef845d214451d7cdf0e0aa9d1
haoxiaoning <1977835153@qq.com>
2019-11-01 17:50:16 +0800

    xqfz

--- 正则/re_test_3.py
+++ 正则/re_test_3.py
@@ -6,39 +6,37 @@ import re
 
 
 if __name__ == '__main__':
-    # 正预测先行断言: (?=exp)
-    # 匹配一个位置之前的内容, 此位置满足正则exp
-    s = "I'm singing while you're dancing."
-    p = re.compile(r'\b\w+(?=ing\b)')
+    s = 'The fat cat sat on the mat.'
+    # # 正预测先行断言: (?=exp)
+    # # 匹配一个位置之前的内容, 此位置满足正则exp
+    p = re.compile('(The|the)(?=\s.at)')
     print re.findall(p, s)
 
-    # 正回顾后发断言: (?<=exp)
-    # 匹配一个位置之后的文本, 此位置满足正则exp, 结果不包含此位置
-    s = "doing done do todo"
-    p = re.compile(r'(?<=\bdo)\w+\b')
-    print re.findall(p, s)
+    # # 正回顾后发断言: (?<=exp)
+    # # 匹配一个位置之后的文本, 此位置满足正则exp, 结果不包含此位置
 
-    # 负预测先行断言: (?!exp)
-    # 匹配一个位置之前的文本, 此位置不满足正则exp, 结果不包含此位置
-    s = "done run going"  # ?
-    p = re.compile(r'\b\w+(?!ing\b)')
-    print re.findall(p, s)
 
-    # 负回顾后发断言: (?<!exp)
-    # 匹配一个位置之后的文本, 此位置不满足正则exp, 结果不包含此位置
-    s = 'done run going'  # ?
-    p = re.compile(r'(?<!\bdo)\w+\b')
-    print re.findall(p, s)
-
-    # 例子:
-    # 匹配ip地址中的四个整数
-    ip = '192.168.1.123'
-    p = re.compile(r'(?<=\.)?\d+(?=\.)?')
-    print re.findall(p, ip)
-
-    # 匹配字符串s中的一些单词，这些单词不以’x’开头且不以’y’结尾：
-    # ???
-    s = 'xaay xbbc accd'
-    p = re.compile(r'(?<!\bx)\w+(?!y\b)')
-    print re.findall(p, s)
+    # # 负预测先行断言: (?!exp)
+    # # 匹配一个位置之前的文本, 此位置不满足正则exp, 结果不包含此位置
+    # s = "done run going"  # ?
+    # p = re.compile(r'\b\w+(?!ing\b)')
+    # print re.findall(p, s)
+    #
+    # # 负回顾后发断言: (?<!exp)
+    # # 匹配一个位置之后的文本, 此位置不满足正则exp, 结果不包含此位置
+    # s = 'done run going'  # ?
+    # p = re.compile(r'(?<!\bdo)\w+\b')
+    # print re.findall(p, s)
+    #
+    # # 例子:
+    # # 匹配ip地址中的四个整数
+    # ip = '192.168.1.123'
+    # p = re.compile(r'(?<=\.)?\d+(?=\.)?')
+    # print re.findall(p, ip)
+    #
+    # # 匹配字符串s中的一些单词，这些单词不以’x’开头且不以’y’结尾：
+    # # ???
+    # s = 'xaay xbbc accd'
+    # p = re.compile(r'(?<!\bx)\w+(?!y\b)')
+    # print re.findall(p, s)
 
